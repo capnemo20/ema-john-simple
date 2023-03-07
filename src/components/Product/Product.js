c23bce8e04b48cbd6c7f73494811570cfe55f448
capnemo20 <z.sujon20@outlook.com>
2023-03-07 16:31:44 +0000

    destructure directly and how to wrap

--- src/components/Product/Product.js
+++ src/components/Product/Product.js
@@ -1,10 +1,18 @@
 import React from 'react';
 import "./Product.css";
 
+// <<----------------------------------------->>
+// destructure directly without using props
+// const Product =(product, handleAddToCart)()=>{
+    // const {handleAddToCart} = props;
+    // const {name, img, seller, price, ratings} = product;
+// }
+// <<----------------------------------------->>
+
 const Product = (props) => {
     // const {handleAddToCart} = props;
      const {name, img, seller, price, ratings} = props.product;
-     console.log(props);
+    
 
     return (
         <div className = "product">
@@ -16,7 +24,10 @@ const Product = (props) => {
                 <p><small>seller: {seller}</small></p> 
                 <p><small>ratings: {ratings} stars</small></p>
 
-            </div>
+            </div>              
+            {/* <button onClick={()=>handleAddToCart(product)} className="btn-cart">
+                <p>Add To Cart</p>
+            </button> */}
             <button onClick={()=>props.handleAddToCart(props.product)} className="btn-cart">
                 <p>Add To Cart</p>
             </button>
